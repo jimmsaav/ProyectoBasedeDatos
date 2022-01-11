caf634ca1ded9336549c9fe41d247888e1483ad6
eamatute <74827981+eamatute@users.noreply.github.com>
2022-01-10 23:56:41 -0500

    Update consultas.sql

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -1,4 +1,5 @@
 use tliving;
+/*Parte 1*/
 select * from actividad
 where idActividad = 1;
 
@@ -25,7 +26,8 @@ where idUsuario = 8;
 
 select * from usuario_bloqueado
 where id_bloqueo = 9;
---------------------------------
+
+/*Parte 2*/
 select * from actividad
 where tipo = 'Deporte';
 
@@ -52,3 +54,45 @@ where ciudad = 'Cuenca';
 
 select * from usuario_bloqueado
 where idUsuarioBloquea = 5;
+
+/*Parte 3*/
+/*Proceso 15: Listar amigos*/
+SELECT idAmistad, idUsuario1, idUsuario2
+FROM AMISTAD
+WHERE idUsuario1 = 1 || idUsuario2 = 1;
+
+/*Proceso 16: Listar amigos recientes*/
+SELECT idAmistad, idUsuario1, idUsuario2, fecha_envio_amistad
+FROM AMISTAD
+WHERE idUsuario1 = 1 || idUsuario2 = 1
+AND fecha_envio_amistad < '2021-04-04' && fecha_envio_amistad > '2021-04-02';
+
+/*Proceso 17: Buscar amigos por apellido*/
+SELECT *
+FROM AMISTAD 
+WHERE (idUsuario1 in (
+	SELECT idUsuario
+	FROM USUARIO
+	WHERE Apellido like "Lopez"
+) AND idUsuario2 = 1) OR (idUsuario2 in (
+	SELECT idUsuario
+	FROM USUARIO
+	WHERE Apellido like "Lopez")
+	AND idUsuario1 = 1
+);
+
+/*Proceso 18: Consultar Datos del usuario*/
+select * from usuario
+where nombre = 'Juan' and apellido = 'Hidalgo';
+
+/*proceso 19: Consultar Lista actividades del usuario*/
+ select actividad.nombre, actividad.tipo, actividad.descripcion, sitio.nombre as Lugar_de_actividad
+ from usuario, asistencia, sitio, actividad
+ where usuario.idUsuario = asistencia.idUsuario and asistencia.idSitio = sitio.idSitio and sitio.idActividad = actividad.idActividad and
+ usuario.nombre= 'Juan' and usuario.apellido = 'Hidalgo';
+ 
+ /*proceso 20: Consultar compatibilidad de actividades*/
+  select usuario.nombre, usuario.apellido, usuario.direccion
+ from usuario, asistencia, sitio, actividad
+ where usuario.idUsuario = asistencia.idUsuario and asistencia.idSitio = sitio.idSitio and sitio.idActividad = actividad.idActividad and
+ actividad.nombre = 'Parchis';
