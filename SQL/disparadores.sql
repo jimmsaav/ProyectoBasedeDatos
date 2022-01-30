e1a422a77418121ff58f4bf304ad4948f5bd68f2
Xavier Cobos <86490191+xxcobos@users.noreply.github.com>
2022-01-30 17:00:55 -0500

    Update disparadores.sql

--- SQL/disparadores.sql
+++ SQL/disparadores.sql
@@ -1,8 +1,9 @@
-##Proceso numero 1 ... Cambiar entre usuario bloqueado y alterar tabla de amistad 
+
+##Proceso numero 1 ... Cambiar amigo a bloqueado y actualizar tabla Amistad
 
 
 DELIMITER $$
-CREATE TRIGGER 'actualizarAmigo'
+CREATE TRIGGER 'actualizarBloqueado'
 AFTER INSERT ON 'USUARIO_BLOQUEADO' 
 FOR EACH ROW
 BEGIN
@@ -11,3 +12,28 @@ BEGIN
 
 END$$
 DELIMITER ;
+
+
+
+
+
+
+
+
+##Proceso numero 2 ... Cambiar amigo bloqueado a nuevamente en lista de amigos -- Cambio de bloqueado a Amistad
+
+
+DELIMITER $$
+CREATE TRIGGER 'actualizarAmigo'
+AFTER INSERT ON 'Amistad' 
+FOR EACH ROW
+BEGIN
+ 	
+	delete from USUARIO_BLOQUEADO ub where new.idUsuario1 = ub.idUsuarioBloquea and ub.idUsuarioBloqueado = new.idUsuario2;
+
+END$$
+DELIMITER ;
+
+
+
+
