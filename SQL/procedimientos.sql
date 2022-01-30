d9b6fc509368c9ec9520b4662e78778731f7bc96
Xavier Cobos <86490191+xxcobos@users.noreply.github.com>
2022-01-30 13:40:10 -0500

    Update procedimientos.sql

--- SQL/procedimientos.sql
+++ SQL/procedimientos.sql
@@ -9,7 +9,7 @@ drop procedure if exists mandar_mensaje;
 
 DELIMETER //
 
-create procedure mandar_mensaje (in Face int, in Usuario_Agregado INT, in texto_mensaje varchar(1000), tema varchar(50), in datos_horario datetime)
+create procedure mandar_mensaje (in Face int, in Usuario_Agregado INT, in texto_mensaje varchar(1000), in tema varchar(50), in esta varchar(15), in datos_horario datetime)
 
 begin
 DECLARE suma int; 
@@ -28,8 +28,8 @@ select ub.id_bloqueo into bloqueado from USUARIO_BLOQUEADO ub where Face = ub.id
 
 		select idMensaje into suma from mensaje group by idMensaje desc limit 1; 
 		set @suma = @suma + 1 ;  
-		insert into mensaje(idMensaje, fecha, contenido, asunto, idReceptor, idEmisor) 
-		values (suma, datos_horario, texto_mensaje, tema, Usuario_Agregado, Face);
+		insert into mensaje(idMensaje, fecha, contenido, asunto, estado, idReceptor, idEmisor) 
+		values (suma, datos_horario, texto_mensaje, tema, esta, Usuario_Agregado, Face);
 			commit;
 		else
 			rollback;
@@ -43,6 +43,7 @@ select ub.id_bloqueo into bloqueado from USUARIO_BLOQUEADO ub where Face = ub.id
 END //
 
 DELIMITER;
+
 
 
 
