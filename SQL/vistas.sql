471ab30fd35b038c73b5e344551dd2b85e507551
Kevin Zhang <kazp058@gmail.com>
2022-01-30 17:30:43 -0500

    Se agrego reporte de usuarios por ciudad

--- SQL/vistas.sql
+++ SQL/vistas.sql
@@ -5,3 +5,7 @@ create view preferenciasPorUsuario as
 	select u.idUsuario, s.idActividad from usuario as u left join asistencia as a on u.idUsuario = a.idUsuario
 													  left join sitio as s on a.idSitio = s.idSitio;
 
+drop view if exists usuariosPorCiudad;
+create view usuariosPorCiudad as 
+	select ciudad, count(*) from usuario group by ciudad;
+    
